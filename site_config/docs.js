b3a6d2c33f03cf66d8358eee1ae3c64391e1f761
wangxianji <89560366@qq.com>
2019-09-10 17:41:10 +0800

    change url link

--- site_config/docs.js
+++ site_config/docs.js
@@ -36,23 +36,23 @@ export default {
         children: [
           {
             title: '关于我们',
-            link: '/en-us/docs/aboutus.html',
+            link: '/zh-cn/docs/aboutus.html',
           },
           {
             title: '服务',
-            link: '/en-us/docs/services.html',
+            link: '/zh-cn/docs/services.html',
           },
           {
             title: '团队',
-            link: '/en-us/docs/team.html',
+            link: '/zh-cn/docs/team.html',
           },
           {
             title: '联系我们',
-            link: '/en-us/docs/contactus.html',
+            link: '/zh-cn/docs/contactus.html',
           },
           {
             title: '加入我们',
-            link: '/en-us/docs/joinus.html',
+            link: '/zh-cn/docs/joinus.html',
           }
         ],
       },
